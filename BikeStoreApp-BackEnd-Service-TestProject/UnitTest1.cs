f1388e991aa64fc931679aecddf9eebe47107aa0
geethasamynathan <geethasamynathan2011@gmail.com>
2024-10-12 15:22:12 +0530

    Add project files.

---
+++ BikeStoreApp-BackEnd-Service-TestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BikeStoreApp_BackEnd_Service_TestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ BikeStoreApp-FrontEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BikeStoreApp_FrontEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
